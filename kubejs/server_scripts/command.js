dfbee96385708ee7886053010bdda7a147a73a4b
KL_jiana <2436262516@qq.com>
2024-07-28 00:46:18 +0800

    command studying EP.1

--- kubejs/server_scripts/command.js
+++ kubejs/server_scripts/command.js
@@ -1,1 +1,19 @@
 // 写packmod的指令快捷设定
+let mode = ["dimensionStages"]
+ServerEvents.commandRegistry(event => {
+    const { commands, arguments } = event
+
+    event.register(commands.literal('packmode').requires(s => s.hasPermission(2))
+      .then(commands.literal('dimensionStages').executes(fly()))
+    )
+    
+    /**
+     * @param {string} run 
+     * @param {Internal.ServerLevel_} server 
+     */
+    let fly = (run, server) => {
+        server.runCommandSilent(`/kubejs packmode ${run}`)
+        server.runCommandSilent(`/reload`)
+        return 1;
+    }
+  })
